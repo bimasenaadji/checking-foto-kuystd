333962cd8a6f21fb73a6b7b0036daff07eec0ab4
bimasenaadji <bimasenaadji@gmail.com>
2026-04-04 10:46:31 +0700

    feat: refactor createClient function to internally handle cookieStore retrieval

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -4,9 +4,10 @@ import { cookies } from "next/headers";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
-export const createClient = (
-  cookieStore: Awaited<ReturnType<typeof cookies>>,
-) => {
+export async function createClient() {
+  // Ambil cookieStore secara internal di sini
+  const cookieStore = await cookies();
+
   return createServerClient(supabaseUrl!, supabaseKey!, {
     cookies: {
       getAll() {
@@ -18,11 +19,9 @@ export const createClient = (
             cookieStore.set(name, value, options),
           );
         } catch {
-          // The `setAll` method was called from a Server Component.
-          // This can be ignored if you have middleware refreshing
-          // user sessions.
+          // Normal jika dipanggil dari Server Component
         }
       },
     },
   });
-};
+}

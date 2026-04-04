4f3ae95ce1fe42a2f1f2daa3735b8ef9e4168579
bimasenaadji <bimasenaadji@gmail.com>
2026-04-04 10:47:57 +0700

    feat: update AuthLayout to use logo image instead of icon

--- components/auth/auth-layout.tsx
+++ components/auth/auth-layout.tsx
@@ -1,12 +1,19 @@
+// src/components/auth/auth-layout.tsx
 import { Camera } from "lucide-react";
+import Image from "next/image";
 
 export function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-background flex flex-col items-center justify-center p-4">
       <div className="w-full max-w-sm">
         <div className="flex flex-col items-center mb-8">
-          <div className="w-14 h-14 bg-primary rounded-2xl flex items-center justify-center mb-4 shadow-lg shadow-primary/25">
-            <Camera className="w-7 h-7 text-primary-foreground" />
+          <div className="w-18 h-18 rounded-2xl  flex items-center justify-center mb-4 shadow-lg shadow-primary/25">
+            <Image
+              src={"/images/kuy_studio_logo.jpg"}
+              alt="Logo Kuy Studio"
+              width={56}
+              height={56}
+            />
           </div>
           <h1 className="text-2xl font-bold text-foreground tracking-tight">
             Kuy Studio
